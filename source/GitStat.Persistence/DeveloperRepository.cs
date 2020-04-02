46998c6275f340110ac60e619e2190a3f739806d
KienbMi <m_kienberger@gmx.net>
2020-04-02 19:27:27 +0200

    improvement getdeveloperstatistik

--- source/GitStat.Persistence/DeveloperRepository.cs
+++ source/GitStat.Persistence/DeveloperRepository.cs
@@ -15,52 +15,19 @@ namespace GitStat.Persistence
 
         public (string Name, int Commits, int FileChanges, int Insertions, int Deletions)[] GetDeveloperStatistik()
         {
-            return _dbContext.Developers.Join(_dbContext.Commits,
-                        developer => developer.Id,
-                        commit => commit.Developer.Id,
-                        (developer, commit) =>
-                        new
-                        {
-                            developer.Id,
-                            developer.Name,
-                            commit.FilesChanges,
-                            commit.Insertions,
-                            commit.Deletions
-
-                        })
-                        .GroupBy(developer => developer.Name)
-                        .Select(d => new
-                        {
-                            Name = d.Key,
-                            Commits = d.Count(),
-                            FileChanges = d.Sum(c => c.FilesChanges),
-                            Insertions = d.Sum(c => c.Insertions),
-                            Deletions = d.Sum(c => c.Deletions)
-                        })
-                        .OrderByDescending(d => d.Commits)
-                        .AsEnumerable()
-                        .Select(developer => (developer.Name, developer.Commits, developer.FileChanges, developer.Insertions, developer.Deletions))
-                        .ToArray();
-
-
-
-
-            //return _dbContext.Commits
-            //           //.Include(d => d.Commits)
-            //           //.ToArray()
-            //           .GroupBy(c => c.DeveloperId)
-            //           .Select(d => new {
-            //                   Name = "Hansi",
-            //                   Commits = d.Count(),
-            //                   FileChanges = d.Sum(c => c.FilesChanges),
-            //                   Insertions = d.Sum(c => c.Insertions),
-            //                   Deletions = d.Sum(c => c.Insertions)
-            //           })
-            //           .ToArray()
-            //           .OrderByDescending(d => d.Commits)
-            //           .AsEnumerable()
-            //           .Select(developer => (developer.Name, developer.Commits, developer.FileChanges, developer.Insertions, developer.Deletions))
-            //           .ToArray();
+            return _dbContext.Developers
+                .Select(d => new
+                {
+                    Name = d.Name,
+                    Commits = d.Commits.Count(),
+                    FileChanges = d.Commits.Sum(c => c.FilesChanges),
+                    Insertions = d.Commits.Sum(c => c.Insertions),
+                    Deletions = d.Commits.Sum(c => c.Deletions)
+                })
+                .OrderByDescending(d => d.Commits)
+                .AsEnumerable()
+                .Select(developer => (developer.Name, developer.Commits, developer.FileChanges, developer.Insertions, developer.Deletions))
+                .ToArray();
         }
     }
 }
